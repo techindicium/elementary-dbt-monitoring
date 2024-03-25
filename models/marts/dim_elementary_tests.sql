80c28d2c6aee6d748c80551cfa15722b1b6bc375
Jessica Oliveira <jesslira23@gmail.com>
2024-03-25 13:25:00 -0300

    Adding model_name column in the dim_elementary_tests

--- models/marts/dim_elementary_tests.sql
+++ models/marts/dim_elementary_tests.sql
@@ -20,11 +20,40 @@ with
             , test_generated_at
         from {{ ref('stg_elementary_dbt_tests') }}
     )
+    , stg_models as (
+        select distinct
+            model_id
+            , model_name
+        from {{ ref('stg_elementary_dbt_models') }}
+    )
+    , joined_tests_models as (
+        select
+            stg_tests.test_id
+            , stg_tests.project_database_name
+            , stg_tests.schema_name
+            , stg_models.model_name
+            , stg_tests.test_name
+            , stg_tests.test_short_name
+            , stg_tests.test_type_mod
+            , stg_tests.test_column_name
+            , stg_tests.test_severity
+            , stg_tests.test_tags
+            , stg_tests.test_depends_on_macros
+            , stg_tests.test_depends_on_nodes
+            , stg_tests.parent_model_unique_id
+            , stg_tests.test_description
+            , stg_tests.package_name
+            , stg_tests.test_type
+            , stg_tests.dbt_test_path
+            , stg_tests.test_generated_at
+        from stg_tests
+        left join stg_models on stg_tests.parent_model_unique_id = stg_models.model_id
+    )
     , stg_tests_with_sk as (
         select distinct
             {{ dbt_utils.surrogate_key(['test_id']) }} as test_sk
             , *
-        from stg_tests
+        from joined_tests_models
     )
 select *
 from stg_tests_with_sk
